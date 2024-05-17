ba8d48be7a0823a131ddcaf120c5a90e2490219b
GiangHM <36400582+GiangHM@users.noreply.github.com>
2024-05-17 21:20:47 +0700

    Correction

--- PracticalAPI/Services/Welcoming.cs
+++ PracticalAPI/Services/Welcoming.cs
@@ -7,19 +7,13 @@ namespace PracticalAPI.Services
     /// Using primary constructor of C# 12
     /// </summary>
     /// <param name="greeting"></param>
-    public class Welcoming([FromKeyedServices("FormalGreeting")] IGreeting greeting
-        , IKeyedServiceProvider keyedServiceProvider)
+    public class Welcoming([FromKeyedServices("FormalGreeting")] IGreeting greeting)
         : IWelcoming
     {
         
         public string Welcome(string name, string message)
         {
             return $"{greeting.Hello(name)}, {message}";
-        }
-        public string Welcome2(string name, string type)
-        {
-            var service = keyedServiceProvider.GetRequiredKeyedService<IGreeting>(type);
-            return service.Hello(name);
         }
     }
 }
