446502ecf23a07aa2347490d1da74a2f72d6615d
GiangHM <hoang-giang.mai@bollore.com>
2024-05-21 11:47:57 +0700

    Implement custom middleware

---
+++ PracticalAPI/CustomMiddleware/CustomExceptionHandlerMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace PracticalAPI.CustomMiddleware
+{
+    /// <summary>
+    /// Step 1: Create a middleware class
+    /// </summary>
+    public class CustomExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        ILogger<CustomExceptionHandlerMiddleware> _logger;
+        public CustomExceptionHandlerMiddleware(RequestDelegate next
+            , ILogger<CustomExceptionHandlerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error while processing your request");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            return context.Response.WriteAsync(exception.Message);
+        }
+    }
+
+    /// <summary>
+    /// Step 2: Create a extension method to use our middleware
+    /// </summary>
+
+    public static class CustomExceptionHandlerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomExceptionHandlers(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<CustomExceptionHandlerMiddleware>();
+        }
+    }
+}
