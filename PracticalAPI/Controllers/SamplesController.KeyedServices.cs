e3ff21a7ef9b078e8b55d10067d90b2bb79f2168
GiangHM <hoang-giang.mai@bollore.com>
2024-12-18 11:55:45 +0700

    Enable Rate limit for some action methods

--- PracticalAPI/Controllers/SamplesController.KeyedServices.cs
+++ PracticalAPI/Controllers/SamplesController.KeyedServices.cs
@@ -1,15 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
 
 namespace PracticalAPI.Controllers
 {
     public partial class SamplesController
     {
+        [EnableRateLimiting("CustomPolicy")]
         [HttpGet]
         [Route("Greeting")]
         public ActionResult<string> Greet1()
         {
             return Ok(_welcoming.WelcomeFormalWay("Victor", "Hope you're doing well"));
         }
+
+        [EnableRateLimiting("User")]
         [HttpGet]
         [Route("Greeting2")]
         public ActionResult<string> Greet2()
